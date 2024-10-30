d2b44c9f7a8995022bddbb17817fdea586d6977a
Estela Padilla <epadgom1210@g.educaand.es>
2024-10-30 10:02:27 +0100

    nueva clase prueba3

--- src/es/iesoretania/proyectoeclipse2/datos/prueba1.java
+++ src/es/iesoretania/proyectoeclipse2/datos/prueba1.java
@@ -3,7 +3,6 @@ package es.iesoretania.proyectoeclipse2.datos;
 public class prueba1 {
 	public static void main(String[] args) {
 		
-		
 		System.out.println("Hola mundo!!!");
 		System.out.println("Hola mundo!!!");
 		System.out.println("pepejuanguille");
