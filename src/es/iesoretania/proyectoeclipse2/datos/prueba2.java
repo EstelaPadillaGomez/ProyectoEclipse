ce386395df23df64cebeb83473b3c364cf110b1d
Estela Padilla <epadgom1210@g.educaand.es>
2024-10-30 09:25:16 +0100

    commit version1

--- src/es/iesoretania/proyectoeclipse2/datos/prueba2.java
+++ src/es/iesoretania/proyectoeclipse2/datos/prueba2.java
@@ -1,11 +1,11 @@
 package es.iesoretania.proyectoeclipse2.datos;
 
-public class prueba1 {
+public class prueba2 {
 	public static void main(String[] args) {
 		
 		
 		System.out.println("Hola mundo!!!");
-		
+		System.out.println("Hola mundo!!!");
 
 		System.out.println("Hola mundo!!!");
 
